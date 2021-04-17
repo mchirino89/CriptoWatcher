e944dd6aeee99918b93d6edb1a4d7c92a357ce49
Mauricio Chirino <1657723+mchirino89@users.noreply.github.com>
2021-04-17 10:53:46 -0300

    Initial repo setup

---
+++ CriptoWatcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CriptoWatcher
+//
+//  Created by Mauricio Chirino on 17/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
